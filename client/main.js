16a44dd39d7eb82d1e098a0b463396dda7f8121d
Eugene Bulkin <ebulkin@caltech.edu>
2016-02-27 15:05:23 -0800

    Update pushing to/from API.
    Currently uses local data folder, which isn't available elsewhere in some
    cases... will have to be modified for real use.

--- client/main.js
+++ client/main.js
@@ -1,19 +1,29 @@
+Meteor._debug = (function (super_meteor_debug) {
+  return function (error, info) {
+    if (!(info && _.has(info, 'msg'))) {
+      super_meteor_debug(error, info);
+    }
+  };
+})(Meteor._debug);
+
 var data;
 var dataDep = new Tracker.Dependency();
-
-Meteor.call('getGameLog', "nba-dwyane-wade", function(error, result) {
-  if (error) {
-    console.log(error.reason);
-    return;
-  }
-  var gameLog = result.data.game_logs[0];
-  data = gameLog;
-  dataDep.changed();
-});
 
 Template.stats.helpers ({
   gameLog: function() {
     dataDep.depend();
     return data;
   }
+});
+
+Streamy.onConnect(function() {
+  Streamy.emit("watch", {
+    player: "c5101bd8-bb6b-4a9f-b949-6fc4f0a33c9e"
+  });
+});
+
+Streamy.on("stats", function(d) {
+  console.log(d);
+  data = d;
+  dataDep.changed();
 });
--- server/main.js
+++ server/main.js
@@ -1,17 +1,38 @@
 var BASE_URL = "https://www.stattleship.com/";
 
-Meteor.startup(function () {
-  // code to run on server at startup
-  // set up search bar
-    var ACCESS_TOKEN = JSON.parse(Assets.getText('api.json')).access_token;
-    HEADERS = {
-      "Content-Type": "application/json",
-      "Accept": "application/vnd.stattleship.com; version=1",
-      "Authorization": "Token token=" + ACCESS_TOKEN
-    };
-});
+var curTime = 1456604373;
+var gameId = "45ff34db-72c7-43ee-9136-da2b260237af";
+var timer;
+var watchedGames = {};
+
+var clientWatch = function(data, socket) {
+  watchedGames[gameId] = [{
+    socket: socket,
+    watching: data
+  }];
+}
 
 Meteor.methods ({
+  'update': function() {
+    while(true) {
+      try {
+        Assets.getText("data/" + gameId + "/" + curTime + ".json");
+        break;
+      } catch(e) {
+        curTime++;
+      }
+    }
+    var result = JSON.parse(Assets.getText("data/" + gameId + "/" + curTime + ".json"));
+    curTime += 7;
+    for(var k in watchedGames) {
+      watchedGames[k].forEach(function(watchData) {
+        var r = result.game_logs.filter(function(log) {
+          return log.player_id == watchData.watching.player;
+        })[0];
+        Streamy.emit("stats", r, watchData.socket);
+      });
+    }
+  },
   'getGameLog' : function (playerId) {
     var sport = "basketball";
     var league = "nba";
@@ -22,10 +43,28 @@ Meteor.methods ({
       var result = HTTP.call("GET",
                              url + "?status=in_progress&player_id=" + playerId,
                              {headers: HEADERS});
-      return result;
+      return result.data;
     } catch (e) {
       console.log(e);
       return null;
     }
   }
 });
+
+Meteor.startup(function () {
+    // code to run on server at startup
+    // set up search bar
+    var ACCESS_TOKEN = JSON.parse(Assets.getText('api.json')).access_token;
+    HEADERS = {
+      "Content-Type": "application/json",
+      "Accept": "application/vnd.stattleship.com; version=1",
+      "Authorization": "Token token=" + ACCESS_TOKEN
+    };
+
+    timer = Meteor.setInterval(function() {
+      Meteor.call("update");
+    }, 500);
+
+    Streamy.on("watch", clientWatch);
+});
+
